7af2db3747c85f4ccc5b8f5c6280d0ebbb6fb0fb
Camilo-Sanchez144 <Camilos0704@gmail.com>
2024-11-12 21:05:36 -0500

    Update TipoPersonaServiceImp.java

--- src/main/java/co/edu/EntidadFinanciera/domain/TipoPersonaServiceImp.java
+++ src/main/java/co/edu/EntidadFinanciera/domain/TipoPersonaServiceImp.java
@@ -7,35 +7,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class TipoPersonaServiceImp implements TipoPersonaService {
 
-    @Autowired
-    private TipoPersonaRepository tp_repositorio;
-
-    @Override
-    public List<TipoPersona> listar() {
-        return tp_repositorio.findAll();
-    }
-    
-    @Override
-    public TipoPersona listarId(int id) {
-        return tp_repositorio.findById(id);
-    }
-
-    @Override
-    public TipoPersona add(TipoPersona p) {
-        return tp_repositorio.save(p);
-    }
-    
-    @Override
-    public TipoPersona edit(TipoPersona p) {
-        return tp_repositorio.save(p);
-    }
-
-    @Override
-    public TipoPersona delete(int id) {
-        TipoPersona tipopersona = tp_repositorio.findById(id);
-        if (tipopersona != null) {
-            tp_repositorio.delete(tipopersona);
-        }
-        return tipopersona;
+  
     }
 }
