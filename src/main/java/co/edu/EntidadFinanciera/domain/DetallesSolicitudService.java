1ee1a88c213b49a55be29d132905ed03adf65a22
Camilo-Sanchez144 <Camilos0704@gmail.com>
2024-11-12 20:54:50 -0500

    Update DetallesSolicitudService.java

--- src/main/java/co/edu/EntidadFinanciera/domain/DetallesSolicitudService.java
+++ src/main/java/co/edu/EntidadFinanciera/domain/DetallesSolicitudService.java
@@ -5,15 +5,6 @@ import java.util.List;
 
 public interface DetallesSolicitudService {
     
-        List<DetallesSolicitud> listar();
 
-    DetallesSolicitud listarId(int id);
-
-    DetallesSolicitud add(DetallesSolicitud p);
-
-    DetallesSolicitud edit(DetallesSolicitud p);
-
-    DetallesSolicitud delete(int id);
-    
     
 }
