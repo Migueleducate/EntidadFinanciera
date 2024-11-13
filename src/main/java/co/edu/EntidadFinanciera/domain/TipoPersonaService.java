5ae5c178ead19b2abec75f691658c32d215c3257
Camilo-Sanchez144 <Camilos0704@gmail.com>
2024-11-12 21:05:17 -0500

    CRUD TipoPersona
    Esta interfaz define métodos CRUD para gestionar objetos de tipo TipoPersona

--- src/main/java/co/edu/EntidadFinanciera/domain/TipoPersonaService.java
+++ src/main/java/co/edu/EntidadFinanciera/domain/TipoPersonaService.java
@@ -3,5 +3,9 @@ package co.edu.EntidadFinanciera.domain;
 import java.util.List; 
  
 public interface TipoPersonaService { 
-
+List<TipoPersona> listar(); 
+TipoPersona listarId(int id); 
+TipoPersona add(TipoPersona p); 
+TipoPersona edit(TipoPersona p); 
+TipoPersona delete(int id);
 }
