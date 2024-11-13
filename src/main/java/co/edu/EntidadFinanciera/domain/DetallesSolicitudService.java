bc82c39e21e73ef7cf79bf53ab7ca2b6dad7a345
Camilo-Sanchez144 <Camilos0704@gmail.com>
2024-11-12 20:56:56 -0500

    CAMBIOS
    Cambio en detalle Solicitud Service

--- src/main/java/co/edu/EntidadFinanciera/domain/DetallesSolicitudService.java
+++ src/main/java/co/edu/EntidadFinanciera/domain/DetallesSolicitudService.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 public interface DetallesSolicitudService {
     
+    List<DetallesSolicitud> listar();
 
+    DetallesSolicitud listarId(int id);
+
+    DetallesSolicitud add(DetallesSolicitud p);
+
+    DetallesSolicitud edit(DetallesSolicitud p);
+
+    DetallesSolicitud delete(int id);
+    
     
 }
