6b9674eae32b1314f0849426cd84aa2f313f69a5
Camilo-Sanchez144 <Camilos0704@gmail.com>
2024-11-12 21:04:45 -0500

    Update TipoPersonaService.java

--- src/main/java/co/edu/EntidadFinanciera/domain/TipoPersonaService.java
+++ src/main/java/co/edu/EntidadFinanciera/domain/TipoPersonaService.java
@@ -3,9 +3,5 @@ package co.edu.EntidadFinanciera.domain;
 import java.util.List; 
  
 public interface TipoPersonaService { 
-List<TipoPersona> listar(); 
-TipoPersona listarId(int id); 
-TipoPersona add(TipoPersona p); 
-TipoPersona edit(TipoPersona p); 
-TipoPersona delete(int id);
+
 }
